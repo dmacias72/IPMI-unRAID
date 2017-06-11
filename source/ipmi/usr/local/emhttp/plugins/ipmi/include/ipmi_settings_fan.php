7a623a6d40f23a6b24c01d63603baf4450aaea3c
dmacias72 <derekmacias@live.com>
2017-06-11 01:38:57 -0600

    Add supermicro settings change board info vars

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_settings_fan.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_settings_fan.php
@@ -7,26 +7,28 @@ $fanctrl = isset($fancfg['FANCONTROL']) ? htmlspecialchars($fancfg['FANCONTROL']
 $fanpoll = isset($fancfg['FANPOLL'])    ? intval($fancfg['FANPOLL'])              : 3;
 $fanip   = (isset($fancfg['FANIP']) && ($netsvc === 'enable')) ? htmlspecialchars($fancfg['FANIP']) : htmlspecialchars($ipaddr) ;
 
-/* board files */
-$boards = ['ASRock'=>'','ASRockRack'=>'','Supermicro'=>''];
-$board_file = "$plg_path/board.json";
-$board_file_status = (file_exists($board_file));
-$board_json = ($board_file_status) ? json_decode((file_get_contents($board_file)), true) : [];
-
 /* board info */
-$board_log = '/var/log/ipmiboard';
-if (file_exists($board_log))
-    $board = file_get_contents($board_log);
-else{
-    $board = ($ipmi || !empty($netopts)) ? trim(shell_exec("ipmi-fru $netopts | grep 'Manufacturer' | awk -F 'r:' '{print $2}'")) : 'unknown';
-    if ($board !== 'unknown')
-        file_put_contents($board_log, $board);
+$boards = ['ASRock'=>'','ASRockRack'=>'','Supermicro'=>''];
+$board = trim(shell_exec("dmidecode -t 2 | grep 'Manufacturer' | awk -F 'r:' '{print $2}'"));
+$board_model  = trim(shell_exec("dmidecode -t 2 | grep 'Product Name' | awk -F 'e:' '{print $2}'"));
+$board_status  = array_key_exists($board, $boards);
+if($board !== 'Supermicro'){
+    $board_file = "$plg_path/board.json";
+    $board_file_status = (file_exists($board_file));
+    $board_json = ($board_file_status) ? json_decode((file_get_contents($board_file)), true) : [];
+}else{
+    $board_file_status = true;
+    $board_json = [ 'Supermicro' =>
+            [ 'raw'   => '',
+              'auto'  => '01',
+              'full'  => '64',
+              'fans'  => [
+                'FAN1234' => '',
+                'FANA' => ''
+              ]
+        ]
+    ];
 }
-
-//$board = 'Supermicro';
-
-$board_status = array_key_exists($board, $boards);
-$board_asrock = ($board === 'ASRock' || $board === 'ASRockRack');
 
 // fan network options
 $fanopts = ($netsvc === 'enable') ? '-h '.escapeshellarg($fanip).' -u '.escapeshellarg($user).' -p '.
