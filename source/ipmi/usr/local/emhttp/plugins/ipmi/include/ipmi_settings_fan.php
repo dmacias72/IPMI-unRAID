672106e4f66f3b7291e5e29c55f58ff01e49b34e
dmacias72 <derekmacias@live.com>
2017-10-21 15:52:41 -0600

    check sockets only for non supermicro

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_settings_fan.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_settings_fan.php
@@ -15,16 +15,15 @@ $fanip   = (isset($fancfg['FANIP']) && ($netsvc === 'enable')) ? htmlspecialchar
 $boards        = ['ASRock'=>'','ASRockRack'=>'','Supermicro'=>''];
 $board         = trim(shell_exec("dmidecode -t 2 | grep 'Manufacturer' | awk -F 'r:' '{print $2}'"));
 $board_model   = trim(shell_exec("dmidecode -t 2 | grep 'Product Name' | awk -F 'e:' '{print $2}'"));
-$sockets       = (intval(trim(shell_exec("/usr/bin/lscpu | grep 'Socket(s):' | awk '{print $2}'"))) < 2) ? 0 : 1;
-$socket0       = ($sockets < 1);
 $board_status  = array_key_exists($board, $boards);
 
-
 if($board !== 'Supermicro'){
+    $cmd_count = (intval(trim(shell_exec("/usr/bin/lscpu | grep 'Socket(s):' | awk '{print $2}'"))) < 2) ? 0 : 1;
     $board_file = "$plg_path/board.json";
     $board_file_status = (file_exists($board_file));
     $board_json = ($board_file_status) ? json_decode((file_get_contents($board_file)), true) : [];
 }else{
+    $cmd_count = 0;
     $board_file_status = true;
     $board_json = [ 'Supermicro' =>
             [ 'raw'   => '00 30 70 66 01',
