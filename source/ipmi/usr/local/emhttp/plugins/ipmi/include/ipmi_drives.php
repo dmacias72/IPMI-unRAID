8fe9280184e53f33f74c4f03d53b6c15c4464c2f
dmacias72 <derekmacias@live.com>
2017-10-27 23:24:09 -0600

    get available hard drives from emhttp vars disk.ini and devs.ini

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_drives.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_drives.php
@@ -1,23 +1,20 @@
 <?php
-/* scan directory for type */
-function scan_dir($dir, $type = ""){
-    $out = array();
-    foreach (array_slice(scandir($dir), 2) as $entry){
-        $sep   = (preg_match("/\/$/", $dir)) ? "" : "/";
-        $out[] = $dir.$sep.$entry ;
-    }
-    return $out;
-}
+if(getopt('h') == true)
+    print_r(get_all_hdds());
 
-/* get all hard drives except flash drive */
 function get_all_hdds(){
+    $devs = array_merge(parse_ini_file('/var/local/emhttp/disks.ini',true), parse_ini_file('/var/local/emhttp/devs.ini',true));
+
+    unset($devs['flash']);
     $hdds = array();
-    $flash = preg_replace("/\d$/", "", realpath("/dev/disk/by-label/UNRAID"));
-    foreach (scan_dir("/dev/") as $dev) {
-        if(preg_match("/[sh]d[a-z]+$/", $dev) && $dev != $flash) {
-            $hdds["{$dev}"] = trim(shell_exec("udevadm info --query=property --name={$dev} | grep  ID_SERIAL= | cut -c11-"));
-        }
+
+    foreach ($devs as $dev) {
+        $id = $dev['id'];
+        $device = $dev['device'];
+        if(!empty($id) && !empty($device))
+            $hdds[$device] = $id;
     }
-    return $hdds;
+    ksort($hdds);
+    return array_flip($hdds);
 }
 ?>
