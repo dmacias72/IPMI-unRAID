c5de0db7ec68637881086b36437e46e4903b817f
dmacias72 <derekmacias@live.com>
2017-11-11 11:19:57 -0700

    remove lockfile, use dynamic check

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_check.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/ipmi_check.php
@@ -4,15 +4,4 @@ $boards        = ['ASRock'=>'','ASRockRack'=>'','Supermicro'=>''];
 $board         = trim(shell_exec("dmidecode -t 2 | grep 'Manufacturer' | awk -F 'r:' '{print $2}'"));
 $board_model   = trim(shell_exec("dmidecode -t 2 | grep 'Product Name' | awk -F 'e:' '{print $2}'"));
 $board_status  = array_key_exists($board, $boards);
-
-// create a lockfile for ipmi fan control
-$fanctrl_file = "/boot/config/plugins/ipmi/ipmifan";
-$fanctrl_lock = (file_exists($fanctrl_file));
-if($board_status !== false){
-    if(!$fanctrl_lock)
-        file_put_contents($fanctrl_file, 'Display Fan Control page if this file exists');
-}else{
-    if($fanctrl_lock)
-        unlink($fanctrl_file);
-}
 ?>
