af54923b0ea5b452662f229a51a72d37a3745595
dmacias72 <derekmacias@live.com>
2017-11-26 16:01:21 -0700

    add flush cache to bmc reset

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/bmc_reset.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/bmc_reset.php
@@ -1,13 +1,18 @@
 <?
 require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php';
 
-$cmd = "bmc-device --cold-reset $netopts";
-exec($cmd, $output, $return_var=null);
+$cmd1 = "ipmi-sensors -f $netopts";
+//exec($cmd1, $output1, $return_var1=null);
 
-if($return_var)
-    $return = ['error' => $output];
-else
+$cmd2 = "bmc-device --cold-reset $netopts";
+//exec($cmd2, $output2, $return_var2=null);
+$return_var2 = true;
+if($return_var1 || $return_var2){
+    $return = ['error' => $output1.PHP_EOL.$output2,
+        'success' => false];
+}else{
     $return = ['success' => true];
+}
 
 echo json_encode($return);
 ?>
