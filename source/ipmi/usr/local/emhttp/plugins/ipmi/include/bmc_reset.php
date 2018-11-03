fa495b8ff3d7a289297656580783e77850bb42cb
dmacias72 <derekmacias@live.com>
2018-11-03 11:20:06 -0600

    remove test code

--- source/ipmi/usr/local/emhttp/plugins/ipmi/include/bmc_reset.php
+++ source/ipmi/usr/local/emhttp/plugins/ipmi/include/bmc_reset.php
@@ -2,11 +2,11 @@
 require_once '/usr/local/emhttp/plugins/ipmi/include/ipmi_options.php';
 
 $cmd1 = "ipmi-sensors -f $netopts";
-//exec($cmd1, $output1, $return_var1=null);
+exec($cmd1, $output1, $return_var1);
 
 $cmd2 = "bmc-device --cold-reset $netopts";
-//exec($cmd2, $output2, $return_var2=null);
-$return_var2 = true;
+exec($cmd2, $output2, $return_var2);
+
 if($return_var1 || $return_var2){
     $return = ['error' => $output1.PHP_EOL.$output2,
         'success' => false];
